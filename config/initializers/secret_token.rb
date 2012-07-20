902725ec539f858d7e511f04889ecb5c5f8d83ac
Josh Smith <joshsmith9255@gmail.com>
2012-07-19 20:50:27 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeaconDatabase::Application.config.secret_token = '904800d054251d02b8e609628e20194585db3299ccf0622839e3ed8d13514949c54784333b0acac1c0f95f5d82f8e5c6d1a105c3c7c9f6c863d6ed69f13847f0'
