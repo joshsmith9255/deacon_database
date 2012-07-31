0defb3cc80e984daba9cf65d087e5257e6631307
Josh Smith <joshsmith9255@gmail.com>
2012-07-31 03:47:07 -0400

    Changed routes, got rid of default index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,25 @@
 DeaconDatabase::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+    # Generated model routes
+    resources :clients
+    resources :deacons
+    resources :assignments
+    #resources :sessions
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
+    # Semi-static page routes
+    match 'home' => 'home#index', :as => :home
+    match 'about' => 'home#about', :as => :about
+    match 'contact' => 'home#contact', :as => :contact
+    match 'privacy' => 'home#privacy', :as => :privacy
+    match 'search' => 'home#search', :as => :search
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    # match 'user/edit' => 'users#edit', :as => :edit_current_user
+    # match 'signup' => 'users#new', :as => :signup
+    # match 'logout' => 'sessions#destroy', :as => :logout
+    # match 'login' => 'sessions#new', :as => :login
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+    # Set the root url
+    root :to => 'home#index'
 
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  end
 
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
-end
